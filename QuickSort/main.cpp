f3ce457a77df5e68f1ef603bb8ee350951678579
MappleTT <1653516704@qq.com>
2019-09-03 14:34:26 +0800

    快速排序

--- QuickSort/main.cpp
+++ QuickSort/main.cpp
@@ -1,4 +1,3 @@
-//此快速排序为普通的快速排序，还有更好的三路快排
 //时间复杂度O(nlogn)
 #include<iostream>
 #include<ctime>
@@ -14,26 +13,26 @@ void Print(int arr[],int n){
 }
 
 
-//方法一
-int quicksort(int arr[],int l,int r){
-
-    //以l这个位置的元素作为参考数
-    int j=l;
-    for(int i=l+1;i<=r;i++){
-        //arr[i]只要比arr[l]这个数小的都交换元素值
-        if(arr[i]<arr[l]){
-            j++;
-            swap(arr[i],arr[j]);
-        }
-    }
-
-    // 最后：arr[j],arr[j+1]...arr[l]...arr[r] 其中arr[j+1]...arr[l-1]都是比arr[l]小的数;
-    // arr[l+1]...ar[r]都是比arr[l]大的数
-    // 交换arr[j]和arr[l]的值
-    swap(arr[j],arr[l]);
-    //返回这个子数组的中间位置的下标
-    return j;
-}
+////方法一
+//int quicksort(int arr[],int l,int r){
+//
+//    //以l这个位置的元素作为参考数
+//    int j=l;
+//    for(int i=l+1;i<=r;i++){
+//        //arr[i]只要比arr[l]这个数小的都交换元素值
+//        if(arr[i]<arr[l]){
+//            j++;
+//            swap(arr[i],arr[j]);
+//        }
+//    }
+//
+//    // 最后：arr[j],arr[j+1]...arr[l]...arr[r] 其中arr[j+1]...arr[l-1]都是比arr[l]小的数;
+//    // arr[l+1]...ar[r]都是比arr[l]大的数
+//    // 交换arr[j]和arr[l]的值
+//    swap(arr[j],arr[l]);
+//    //返回这个子数组的中间位置的下标
+//    return j;
+//}
 
 //方法二
 //传统的快速排序
@@ -54,8 +53,6 @@ int quicksort(int arr[],int l,int r){
     arr[l]=tem;      //arr[r]=tem;
     return l;
 }
-
-
 
 
 void Quicksort(int arr[],int l,int r){
@@ -79,6 +76,47 @@ void QuickSort(int arr[],int n){
 }
 
 
+
+//以上排序为普通的快速排序，下面这种方式采用的是三路快排
+//三路快排
+void Quicksort3(int *arr,int l,int r)
+{
+    if(l>=r)
+        return ;
+    int v=arr[l];
+    int lt=l;
+    int gt=r+1;
+    int i=l+1;
+    while(i<gt)
+    {
+        if(arr[i]<v)           //比v小的移到左边位置
+        {
+            swap(arr[i],arr[lt+1]);
+            i++;
+            lt++;
+        }
+        else if(arr[i]>v)   //比v大的移到右边位置
+        {
+            swap(arr[i],arr[gt-1]);
+            gt--;
+        }
+        else                  //和v相等的不用移动，放在中间位置
+        {
+            i++;
+        }
+    }
+    swap(arr[l],arr[lt]); //这里不要写swap(v,arr[lt]);因为v和arr[l]地址不同，我们要做的是交换数组中两个元素的位置；
+    Quicksort3(arr,l,lt-1);
+    Quicksort3(arr,gt,r);
+}
+
+void QuickSort3(int *arr,int n)
+{
+    Quicksort3(arr,0,n-1);
+}
+
+
+
 int main(){
 
     //给arr数组随机值
@@ -92,8 +130,8 @@ int main(){
 
     //打印出arr数组排序前的元素序列
     Print(arr,n);
-    //调用快速排序
-    QuickSort(arr,n);
+    //调用三路快速排序
+    QuickSort3(arr,n);
     //打印出arr数组排序后的元素序列(这里的排序方式是按照从小到大进行排序的)
     Print(arr,n);
     return 0;
