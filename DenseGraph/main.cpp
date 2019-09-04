35c749346084f872f5cb45bccf8863f39e8f916f
MappleTT <1653516704@qq.com>
2019-09-04 16:30:30 +0800

    稠密图

---
+++ DenseGraph/main.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<cassert>
+using namespace std;
+
+//稠密图
+class DenseGraph{
+private:
+    int n,m;
+    vector<vector<bool> >arr;
+    bool directed;
+
+
+
+public:
+    DenseGraph(int n,bool directed){
+        this->m=0;
+        this->n=n;
+        this->directed=directed;
+        arr=vector<vector<bool> >(n,vector<bool>(n,false));
+    }
+
+
+    ~DenseGraph(){}
+
+    int V(){
+        return n;
+    }
+
+    int E(){
+        return m;
+    }
+
+    //判断是否存在从v到w的边
+    bool hasEdge(int v,int w){
+        assert(v>=0&&v<n);
+        assert(w>=0&&w<n);
+        return arr[v][w];
+    }
+
+    //添加从v到w的边
+    void addEdge(int v,int w){
+        assert(w>=0&&w<n);
+        assert(v>=0&&v<n);
+
+        if(hasEdge(v,w)){
+            return ;
+        }
+        arr[v][w]=true;
+        //如果是无向图
+        if(!directed){
+            arr[w][v]=true;
+        }
+        m++;
+    }
+
+
+    //打印与v节点相邻的所有边
+    void Print(int v){
+        for(int i=0;i<arr[v].size();i++){
+            if(arr[v][i]){
+                cout<<i<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    //稠密图的迭代器
+    class adjIterator{
+    private:
+        int index;
+        DenseGraph &G;
+        int v;
+    public:
+        adjIterator(DenseGraph &g,int v):G(g){
+            this->v=v;
+            this->index=0;
+        }
+
+        //这样就实现了封装性，用户修改不了图中的数据
+        int  begin(){
+            int index=-1;
+            return next();
+        }
+
+        int next(){
+            for(index+=1;index<G.arr[v].size();index++){
+                if(G.arr[v][index]){
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        bool end(){
+            return index>=G.arr[v].size();
+        }
+    };
+
+
+    //连通分量，深度优先遍历
+    class Component{
+    private:
+        DenseGraph &G;
+        bool *visited;   //给某个节点是否被访问了做个标记
+        int *id;        //这个数组来存储某个节点是属于几号连通分量
+        int ccount;    //表示最大连通分量的个数
+
+        void dfs(int k){
+            visited[k]=true;
+            id[k]=ccount;
+            adjIterator adj(G,k);
+            for(int i=adj.begin();!adj.end();i=adj.next()){
+                if(!visited[i])
+                    dfs(i);
+            }
+        }
+
+    public:
+        Component(DenseGraph &g):G(g){
+            visited=new bool [G.V()];
+            id=new int [G.V()];
+            this->ccount=0;
+            for(int i=0;i<G.V();i++){
+                visited[i]=false;
+                id[i]=-1;
+            }
+
+            //访问图中的每个节点
+            for(int i=0;i<G.V();i++){
+                //如果这个节点没有被访问
+                if(!visited[i]){
+                    //进行深度优先遍历，凡是与i这个节点相邻的节点都被访问了，
+                    // 如果不与i这个节点相邻的节点，说明一定是在另一个连通分量里面
+                    dfs(i);
+
+                    ccount++;
+                }
+            }
+        }
+
+        ~Component(){
+            delete []visited;
+            delete []id;
+        }
+
+        int count(){
+            return ccount;
+        };
+    };
+
+};
+int main()
+{
+
+
+
+    return 0;
+}
